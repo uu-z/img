b616acfb2b42be7a296e6c6cd752c8b74370c7a8
yuyuko <ncysatnaf@gmail.com>
2018-11-08 23:25:57 +0800

    update init page

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,7 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  routes: [{
-      path: '/',
-      name: 'Home',
-      component: require('@/views/Home').default
-    },
+  routes: [
     {
       path: '/danboru',
       name: 'danboru',
